0c70942f43b9892068c9280918ac2aba419900e2
hisaitami <hisaitami@gmail.com>
2022-12-22 15:17:50 +0900

    Update dependency quil version

--- sketch20200301a/project.clj
+++ sketch20200301a/project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"] [quil "3.1.0"] [tyrion "0.1.9"]])
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [quil "4.0.0-SNAPSHOT"]
+                 [tyrion "0.1.9"]])
