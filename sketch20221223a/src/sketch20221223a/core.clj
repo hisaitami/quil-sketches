a5720360a315dbeb409b1dad93fc140d0ee3bb62
hisaitami <hisaitami@gmail.com>
2022-12-26 21:36:03 +0900

    Clean up the code

--- sketch20221223a/src/sketch20221223a/core.clj
+++ sketch20221223a/src/sketch20221223a/core.clj
@@ -14,34 +14,24 @@
    (ref-set qrimg
             (q/load-image
              (-> (qrgen/from @qrtxt :size [(q/width) (q/height)])
-                 (qrgen/as-file)))))
-  
-  {:counter 0})
-
-(defn update-state [state]
-  {:counter (+ (:counter state) 0.1)})
+                 (qrgen/as-file))))))
 
 (defn draw-state [state]
   (q/background 240)
-
   (q/no-stroke)
   (doseq [[x y c] (for [x (range 0 (q/width) 5)
                         y (range 0 (q/height) 5)
-                        :let [c (q/get-pixel @qrimg x y)]]
-                    [x y c])]
-    (when (not= c -1)
-      (q/fill 204 102 0)
-      (q/ellipse x y 5 5)
-      (q/fill (rand-int 250) (rand-int 220) (rand-int 240) (rand-int 200))
-      (q/ellipse x y (rand-int 9) (rand-int 9))
-      )))
+                        :let [c (q/get-pixel @qrimg x y)]
+                        :when (not= c -1)] [x y c])]
+    (q/fill 204 102 0)
+    (q/ellipse x y 5 5)
+    (q/fill (rand-int 250) (rand-int 220) (rand-int 240) (rand-int 200))
+    (q/ellipse x y (rand-int 9) (rand-int 9))))
 
 (q/defsketch sketch20221223a
   :title "hello world in QRcode"
   :size [500 500]
   :setup setup
-  :update update-state
   :draw draw-state
   :features [:keep-on-top]
-  :middleware [m/fun-mode]
-  )
+  :middleware [m/fun-mode])
