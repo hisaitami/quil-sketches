296797da9cd262851f0a7f38604a6f8a9ef28b9f
hisaitami <hisaitami@gmail.com>
2024-01-04 22:44:40 +0900

    Use q/set-state! to store QR image into state

--- sketch20221223a/src/sketch20221223a/core.clj
+++ sketch20221223a/src/sketch20221223a/core.clj
@@ -3,26 +3,21 @@
             [quil.middleware :as m]
             [clj.qrgen :as qrgen]))
 
-(def qrtxt (ref "hello world"))
-(def qrimg (ref nil))
-
 (defn setup []
   (q/frame-rate 30)
-  ;(q/color-mode :hsb)
-
-  (dosync
-   (ref-set qrimg
-            (q/load-image
-             (-> (qrgen/from @qrtxt :size [(q/width) (q/height)])
-                 (qrgen/as-file))))))
+  (q/set-state! :qrimg (q/load-image
+                         (-> "hello world"
+                             (qrgen/from :size [(q/width) (q/height)])
+                             (qrgen/as-file)))))
 
 (defn draw-state [state]
   (q/background 240)
   (q/no-stroke)
-  (doseq [[x y c] (for [x (range 0 (q/width) 5)
+  (doseq [[x y _] (for [x (range 0 (q/width) 5)
                         y (range 0 (q/height) 5)
-                        :let [c (q/get-pixel @qrimg x y)]
-                        :when (not= c -1)] [x y c])]
+                        :let [c (q/get-pixel (:qrimg state) x y)]
+                        :when (not= c -1)]
+                    [x y c])]
     (q/fill 204 102 0)
     (q/ellipse x y 5 5)
     (q/fill (rand-int 250) (rand-int 220) (rand-int 240) (rand-int 200))
