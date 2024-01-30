fc2e52b7398063b128e6009bc645ed0b9018df56
hisaitami <hisaitami@gmail.com>
2024-01-30 09:08:34 +0900

    Delete iced-rebel replaced with the common repl-lib

--- sketch20200116a/project.clj
+++ sketch20200116a/project.clj
@@ -4,10 +4,4 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [quil "4.3.1323"]]
-  :profiles {:dev {:dependencies [[com.bhauman/rebel-readline "0.1.4"]
-                                  [nrepl/nrepl "1.1.0"]
-                                  [refactor-nrepl/refactor-nrepl "3.9.1"]
-                                  [cider/cider-nrepl "0.44.0"]
-                                  [com.github.liquidz/iced-nrepl "1.2.480"]]
-                   :aliases {"rebel" ["trampoline" "run" "-m" "rebel-readline.main"]}}})
+                 [quil "4.3.1323"]])
