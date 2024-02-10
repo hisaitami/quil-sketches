cc80b5028c48d9558181c581593c259b2c762a3a
hisaitami <hisaitami@gmail.com>
2024-02-11 03:56:59 +0900

    Clean

--- sketch20200301a/src/quil_minkowski/core.clj
+++ sketch20200301a/src/quil_minkowski/core.clj
@@ -2,29 +2,30 @@
   (:require [quil.core :as q]
             [tyrion.distance :as distance]))
 
-(def size 500)
+(def W 500)
+(def HALF-W (* 0.5 W))
 
-(def half-size (* 0.5 size))
-
-(def step (quot half-size 10))
-
-(defn normalize [x] (* step (quot x step)))
+(defn normalize [x]
+  (let [step (quot HALF-W 10)]
+    (* step (quot x step))))
 
 (defn setup []
   (q/no-loop))
 
 (defn draw []
-  (doseq [y (range size)
-          x (range size)
-          :let [dist (distance/minkowski [half-size half-size] [x y] 3)
-                gray (normalize (* 255 (/ dist size)))]]
+  (doseq [y (range W)
+          x (range W)
+          :let [dist (distance/minkowski [HALF-W HALF-W] [x y] 3)
+                gray (normalize (* 255 (/ dist W)))]]
     (q/stroke gray)
     (q/stroke-weight 1)
     (q/point x y)))
 
 (q/defsketch quil-minkowski
   :title "draw minkowski distance"
-  :size [size size]
+  :size [W W]
   :setup setup
   :draw draw
   :features [:keep-on-top])
+
+(defn -main [])
